ebc3a91de2741fe1002c7057bc3f29f1fbf5fd6d
bobbyhaze <52491932+bobbyhaze@users.noreply.github.com>
2020-12-09 11:43:53 +0100

    added

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ setuptools.setup(
     description="Python package to analyse pSILAC TMT data",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/bobbyhaze/DynaTMT-py",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
